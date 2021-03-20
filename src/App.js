72a6f6efe49896f799871ba762460889cb853053
Shaswat Prabhat <spd1238@gmail.com>
2021-03-20 12:46:55 +0530

    Add support for prettier and a slight onload spring animation.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { animated, useSpring, config } from "react-spring";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Turn on the Pankha!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <animated.div
+          style={useSpring({
+            config: { mass: 50 },
+            opacity: 1,
+            from: { opacity: 0 },
+          })}
         >
-          Learn React
-        </a>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Turn on the Pankha!!!</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </animated.div>
       </header>
     </div>
   );
