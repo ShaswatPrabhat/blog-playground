5e2bc5604377e0a8b058ba0cff5253ccec7a8940
Shaswat Prabhat <spd1238@gmail.com>
2021-03-20 16:19:33 +0530

    Add gfont changes and some text.

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ const App = () => (
     <header className="App-header">
       <animated.div
         style={useSpring({
-          config: { mass: 50 },
+          config: { mass: 50, clamp: true },
           opacity: 1,
           from: { opacity: 0 },
         })}
       >
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Turn on the Pankha!!!</p>
+        <h2>Welcome to my tech blog!</h2>
+        <p>Here I will share my musings and general tech stuff</p>
+        <p>Also I am always hungry</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with me as I eat ?
         </a>
       </animated.div>
     </header>
