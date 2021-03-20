578b6e945e8214728f61e592a62c38a1331da2cc
Shaswat Prabhat <spd1238@gmail.com>
2021-03-20 13:30:17 +0530

    Integrate husky and precommit hooks.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import logo from "./logo.svg";
-import { animated, useSpring, config } from "react-spring";
-import "./App.css";
+import { animated, useSpring } from 'react-spring';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <animated.div
-          style={useSpring({
-            config: { mass: 50 },
-            opacity: 1,
-            from: { opacity: 0 },
-          })}
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Turn on the Pankha!!!</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </animated.div>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <animated.div
+        style={useSpring({
+          config: { mass: 50 },
+          opacity: 1,
+          from: { opacity: 0 },
+        })}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Turn on the Pankha!!!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          Learn React
+        </a>
+      </animated.div>
+    </header>
+  </div>
+);
 
 export default App;
