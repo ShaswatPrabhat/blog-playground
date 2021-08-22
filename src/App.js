ba4f1fa18ec406c84be92643dc9dabd61aff69d7
Shaswat Prabhat <spd1238@gmail.com>
2021-08-22 22:46:50 +0530

    Strip framer motion components to reduce bundle size.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import { motion } from 'framer-motion';
+import { m, LazyMotion, domAnimation } from 'framer-motion';
 import React from 'react';
 import './App.css';
 import BurgerSideBar from './components/burger-side-bar';
 
 const App = () => (
   <div className="App">
-    <BurgerSideBar />
-    <header className="App-header">
-      <motion.div
-        initial={{ scale: 0.85 }}
-        animate={{ scale: 1 }}
-        transition={{ type: 'spring', duration: 1.8, bounce: 0.7 }}
-      >
-        <h2>Welcome to my blog!</h2>
-        <p>Here I will share my musings and general Shaswat stuff</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <LazyMotion features={domAnimation}>
+      <BurgerSideBar />
+      <header className="App-header">
+        <m.div
+          initial={{ scale: 0.85 }}
+          animate={{ scale: 1 }}
+          transition={{ type: 'spring', duration: 1.8, bounce: 0.7 }}
         >
-          Learn React with me as I eat ?
-        </a>
-      </motion.div>
-    </header>
+          <h2>Welcome to my blog!</h2>
+          <p>Here I will share my musings and general Shaswat stuff</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React with me as I read ?
+          </a>
+        </m.div>
+      </header>
+    </LazyMotion>
   </div>
 );
 
